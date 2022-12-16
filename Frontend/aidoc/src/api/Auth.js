07b8b161895fd6398f9bf4b132e7f662f7329ae0
karisDev <karjaakirill@gmail.com>
2022-12-16 10:29:07 +0300

    remove folders

--- Frontend/aidoc/src/api/Auth.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:3000/api/auth/";
-
-const getHeaders = () => {
-  return {
-    Authorization: `Bearer ${localStorage.getItem("jwt")}`,
-  };
-};
--- Frontend/aidoc/src/pages/PricesPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PricesPage = () => {
-  return <div>PricesPage</div>;
-};
-
-export default PricesPage;
