26caac3e8961cc19d60b733231270886ddacf5f3
karisDev <karjaakirill@gmail.com>
2023-01-23 13:28:48 +0300

    mock api

--- Frontend/aidoc/src/api/Auth.js
+++ Frontend/aidoc/src/api/Auth.js
@@ -68,6 +68,7 @@ export const doGetHistory = async () => {
 };
 
 export const doUploadFile = async (file, progress) => {
+  return await mockDoUploadFile(file, progress);
   // upload files one by one and update progress
   const formData = new FormData();
   formData.append("file", file);
@@ -78,4 +79,41 @@ export const doUploadFile = async (file, progress) => {
     },
   });
   return result.data;
+};
+
+export const mockDoUploadFile = async (file, progress) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  for (let i = 1; i <= 10; i++) {
+    progress(i * 10);
+    await new Promise((resolve) => setTimeout(resolve, 150));
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  const isSell = Math.random() > 0.5;
+
+  const sizeToKb = (size) => {
+    return Math.round(size / 1024);
+  };
+
+  return {
+    id: Math.random(),
+    name: file.name,
+    size: sizeToKb(file.size) + " кб",
+    extension: file.name.split(".").pop(),
+    prediction: isSell ? "Договор купли-продажи" : "Договор оказания услуг",
+    sentences: isSell
+      ? [
+          "Договор купли-продажи № 123 от 01.01.2020 г. между ООО «Рога и копыта» и ИП «Иванов Иван Иванович»",
+          "Продавец передает и покупатель принимает в полном объеме на свои риски и на условиях, указанных в настоящем договоре, объект купли-продажи.",
+          "Покупатель обязуется оплатить продавцу купленное им имущество в соответствии с условиями настоящего договора.",
+        ]
+      : [
+          "Стороны договорились о предоставлении услуг по оказанию технической поддержки.",
+          "В соответствии с данным договором, исполнитель обязуется оказывать услуги по содержанию и обслуживанию оборудования.",
+          "Заказчик обязуется оплачивать услуги, оказанные исполнителем в соответствии с условиями данного договора оказания услуг.",
+          "Настоящий договор оказания услуг имеет силу на весь период его действия и регулирует взаимоотношения между сторонами в отношении оказания указанных услуг.",
+        ],
+  };
 };
